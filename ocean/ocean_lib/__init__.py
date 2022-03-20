9996716bfbcd35565d54ca5914fee8653b0d4c0d
Datatera <100047030+DatateraTechnology@users.noreply.github.com>
2022-03-20 16:31:36 +0100

    Add Ocean Protocol files

---
+++ ocean/ocean_lib/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright 2021 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+"""Initialises ocean lib package."""
+
+__author__ = """OceanProtocol"""
+# fmt: off
+__version__ = '0.8.5'
+# fmt: on
