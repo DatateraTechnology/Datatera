a3f5ec0601d338faa7499fb9bf93b599fbe8fe4c
Datatera <100047030+DatateraTechnology@users.noreply.github.com>
2022-03-23 11:59:41 +0100

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def example():
     return 'The value of __name__ is {}'.format(__name__)
+
+@app.route("/spec")
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "Datatera Alpha"
+    return jsonify(swag)
+
+@app.route('/api/docs')
+def get_docs():
+    print('sending docs')
+    return render_template('swaggerui.html')
