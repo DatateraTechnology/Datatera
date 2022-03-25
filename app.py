2f7d337cd7f0df885a5a361dbf69b70cc43e0693
Datatera <100047030+DatateraTechnology@users.noreply.github.com>
2022-03-25 21:24:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from ocean_lib.common.agreements.service_types import ServiceTypes
 from ocean_lib.assets import trusted_algorithms
 from ocean_lib.web3_internal.constants import ZERO_ADDRESS
 from ocean_lib.models.compute_input import ComputeInput
+#import pickle
+#import numpy
+#from matplotlib import pyplot
 
 app = Flask(__name__)
 
@@ -19,11 +22,9 @@ def spec():
     swag['info']['version'] = "v.1.0"
     swag['info']['title'] = "Welcome to Datatera Alpha"
     return jsonify(swag)
-    #return 'The value of __name__ is {}'.format(__name__)
 
 @app.route('/api')
-def get_docs():
-    print('sending docs')
+def get_api():
     return render_template('swaggerui.html')
 
 config = Config('config.ini')
@@ -33,73 +34,46 @@ print(f"config.network_url = '{config.network_url}'")
 print(f"config.metadata_cache_uri = '{config.metadata_cache_uri}'")
 print(f"config.provider_url = '{config.provider_url}'")
 
+#Constants
+Alice_Wallet_Private_Key = "0x5d75837394b078ce97bc289fa8d75e21000573520bfa7784a9d28ccaae602bf8"
+Bob_Wallet_Private_Key = "0xef4b441145c1d0f3b4bc6d61d29f5c6e502359481152f869247c7a4244d45209"
+
+Dataset_Url = "https://raw.githubusercontent.com/trentmc/branin/main/branin.arff"
+Algorithm_Url = "https://raw.githubusercontent.com/trentmc/branin/main/gpr.py"
+
+#Create Wallet for Data Provider
+alice_wallet = Wallet(ocean.web3, Alice_Wallet_Private_Key, 
+config.block_confirmations, config.transaction_timeout)
+
+#Create Wallet for Data Consumer
+bob_wallet = Wallet(ocean.web3, Bob_Wallet_Private_Key, 
+config.block_confirmations, config.transaction_timeout)
+
 #Create Data Provider Wallet
-@app.route("/alpha/1.0/createwallet/<string:wallet_address>", methods=["GET"], endpoint='create_wallet')
-def create_wallet(wallet_address):
-   
-    alice_wallet = Wallet(ocean.web3, wallet_address, 
-    config.block_confirmations, config.transaction_timeout)
+@app.route("/alpha/1.0/createwallet", methods=["GET"], endpoint='create_wallet')
+def create_wallet():
 
     return jsonify(f"alice_wallet.address = '{alice_wallet.address}'")
 
 #Tokenize Dataset
-@app.route("/alpha/1.0/tokenizedataset/<string:wallet_address>", methods=["GET"], endpoint='tokenize_dataset')
-def tokenize_dataset(wallet_address):
-  
-    alice_wallet = Wallet(ocean.web3, wallet_address, 
-    config.block_confirmations, config.transaction_timeout)
+@app.route("/alpha/1.0/tokenizedataset", methods=["GET"], endpoint='tokenize_dataset')
+def tokenize_dataset():
 
     DATA_datatoken = ocean.create_data_token('DATA1', 'DATA1', alice_wallet, blob=ocean.config.metadata_cache_uri)
     DATA_datatoken.mint(alice_wallet.address, to_wei(100), alice_wallet)
 
     return jsonify(f"DATA_datatoken.address = '{DATA_datatoken.address}'")
-  
-#Create Metadata
-@app.route("/alpha/1.0/createmetadata/<string:url>", methods=["GET"], endpoint='create_metadata')
-def create_metadata(url):
-
-    DATA_metadata = {
-    "main": {
-        "type": "dataset",
-        "files": [
-	  {
-	    "url": url,
-	    "index": 0,
-	    "contentType": "text/text"
-	  }
-	],
-	"name": "branin", "author": "Trent", "license": "CC0",
-	"dateCreated": "2019-12-28T10:55:11Z"
-    }
-}
-
-    return jsonify(f"DATA_metadata = '{DATA_metadata}'")
-
-#Create Data Service
-@app.route("/alpha/1.0/createdataservice/<string:wallet_address>", methods=["GET"], endpoint='create_dataservice')
-def create_dataservice(wallet_address):
-
-    DATA_service_attributes = {
-    "main": {
-        "name": "DATA_dataAssetAccessServiceAgreement",
-        "creator": wallet_address,
-        "timeout": 3600 * 24,
-        "datePublished": "2019-12-28T10:55:11Z",
-        "cost": 1.0,
-    }
-}
-    return jsonify(f"DATA_service_attributes = '{DATA_service_attributes}'")
 
 #Publish Metadata
-@app.route("/alpha/1.0/publishmetadata/<string:wallet_address>/<string:url>", methods=["GET"], endpoint='publish_metadata')
-def publish_metadata(wallet_address, url):
-    
+@app.route("/alpha/1.0/publishmetadata", methods=["GET"], endpoint='publish_metadata')
+def publish_metadata():
+
     DATA_metadata = {
     "main": {
         "type": "dataset",
         "files": [
 	  {
-	    "url": url,
+	    "url": Dataset_Url,
 	    "index": 0,
 	    "contentType": "text/text"
 	  }
@@ -111,7 +85,7 @@ def publish_metadata(wallet_address, url):
     DATA_service_attributes = {
     "main": {
         "name": "DATA_dataAssetAccessServiceAgreement",
-        "creator": wallet_address,
+        "creator": Alice_Wallet_Private_Key,
         "timeout": 3600 * 24,
         "datePublished": "2019-12-28T10:55:11Z",
         "cost": 1.0}
@@ -120,12 +94,9 @@ def publish_metadata(wallet_address, url):
     provider_url = DataServiceProvider.get_url(ocean.config)
 
     DATA_compute_service = Service(
-        service_endpoint=provider_url,
-        service_type=ServiceTypes.CLOUD_COMPUTE,
+        service_endpoint = provider_url,
+        service_type = ServiceTypes.CLOUD_COMPUTE,
         attributes=DATA_service_attributes)
-
-    alice_wallet = Wallet(ocean.web3, wallet_address, 
-    config.block_confirmations, config.transaction_timeout)
 
     DATA_datatoken = ocean.create_data_token('DATA1', 'DATA1', alice_wallet, blob=ocean.config.metadata_cache_uri)
     DATA_datatoken.mint(alice_wallet.address, to_wei(100), alice_wallet)
@@ -139,11 +110,8 @@ def publish_metadata(wallet_address, url):
     return jsonify(f"DATA_ddo.did = '{DATA_ddo.did}'")
 
 #Tokenize Algorithm
-@app.route("/alpha/1.0/tokenizealgorithm/<string:wallet_address>", methods=["GET"], endpoint='tokenize_algorithm')
-def tokenize_algorithm(wallet_address):
-
-    alice_wallet = Wallet(ocean.web3, wallet_address, 
-    config.block_confirmations, config.transaction_timeout)
+@app.route("/alpha/1.0/tokenizealgorithm", methods=["GET"], endpoint='tokenize_algorithm')
+def tokenize_algorithm():
 
     ALG_datatoken = ocean.create_data_token('ALG1', 'ALG1', alice_wallet, blob=ocean.config.metadata_cache_uri)
     ALG_datatoken.mint(alice_wallet.address, to_wei(100), alice_wallet)
@@ -151,11 +119,8 @@ def tokenize_algorithm(wallet_address):
     return jsonify(f"ALG_datatoken.address = '{ALG_datatoken.address}'")
 
 #Publish Algorithm
-@app.route("/alpha/1.0/publishalgorithm/<string:wallet_address>", methods=["GET"], endpoint='publish_algorithm')
-def publish_algorithm(wallet_address):
-
-    alice_wallet = Wallet(ocean.web3, wallet_address, 
-    config.block_confirmations, config.transaction_timeout)
+@app.route("/alpha/1.0/publishalgorithm", methods=["GET"], endpoint='publish_algorithm')
+def publish_algorithm():
 
     ALG_datatoken = ocean.create_data_token('ALG1', 'ALG1', alice_wallet, blob=ocean.config.metadata_cache_uri)
     ALG_datatoken.mint(alice_wallet.address, to_wei(100), alice_wallet)
@@ -175,7 +140,7 @@ def publish_algorithm(wallet_address):
         },
         "files": [
 	  {
-	    "url": "https://raw.githubusercontent.com/trentmc/branin/main/gpr.py",
+	    "url": Algorithm_Url,
 	    "index": 0,
 	    "contentType": "text/text",
 	  }
@@ -202,26 +167,23 @@ def publish_algorithm(wallet_address):
         attributes = ALG_service_attributes)
 
     ALG_ddo = ocean.assets.create(
-    metadata=ALG_metadata,
-    publisher_wallet=alice_wallet,
-    services=[ALG_access_service],
-    data_token_address=ALG_datatoken.address)
+    metadata = ALG_metadata,
+    publisher_wallet = alice_wallet,
+    services = [ALG_access_service],
+    data_token_address = ALG_datatoken.address)
 
     return jsonify(f"ALG did = '{ALG_ddo.did}'")
 
 #Authorize Algorithm
-@app.route("/alpha/1.0/authorizealgorithm/<string:bob_wallet_address>/<string:alice_wallet_address>/<string:data_url>/<string:algo_url>", methods=["GET"], endpoint='authorize_algorithm')
-def authorize_algorithm(bob_wallet_address, alice_wallet_address, data_url, algo_url):
-
-    alice_wallet = Wallet(ocean.web3, alice_wallet_address, 
-    config.block_confirmations, config.transaction_timeout)
+@app.route("/alpha/1.0/authorizealgorithm", methods=["GET"], endpoint='authorize_algorithm')
+def authorize_algorithm():
 
     DATA_metadata = {
         "main": {
             "type": "dataset",
             "files": [
         {
-            "url": data_url,
+            "url": Dataset_Url,
             "index": 0,
             "contentType": "text/text"
         }
@@ -234,16 +196,18 @@ def authorize_algorithm(bob_wallet_address, alice_wallet_address, data_url, algo
     DATA_service_attributes = {
     "main": {
         "name": "DATA_dataAssetAccessServiceAgreement",
-        "creator": alice_wallet_address,
+        "creator": alice_wallet.address,
         "timeout": 3600 * 24,
         "datePublished": "2019-12-28T10:55:11Z",
         "cost": 1.0,
         }
     }
 
+    provider_url = DataServiceProvider.get_url(ocean.config)
+
     DATA_compute_service = Service(
-        service_endpoint=provider_url,
-        service_type=ServiceTypes.CLOUD_COMPUTE,
+        service_endpoint = provider_url,
+        service_type = ServiceTypes.CLOUD_COMPUTE,
         attributes=DATA_service_attributes)
 
     DATA_datatoken = ocean.create_data_token('DATA1', 'DATA1', alice_wallet, blob=ocean.config.metadata_cache_uri)
@@ -273,7 +237,7 @@ def authorize_algorithm(bob_wallet_address, alice_wallet_address, data_url, algo
         },
         "files": [
 	  {
-	    "url": algo_url,
+	    "url": Algorithm_Url,
 	    "index": 0,
 	    "contentType": "text/text",
 	  }
@@ -293,44 +257,35 @@ def authorize_algorithm(bob_wallet_address, alice_wallet_address, data_url, algo
             }
         }
 
-    provider_url = DataServiceProvider.get_url(ocean.config)
-
     ALG_access_service = Service(
         service_endpoint = provider_url,
         service_type = ServiceTypes.CLOUD_COMPUTE,
         attributes = ALG_service_attributes)
 
     ALG_ddo = ocean.assets.create(
-    metadata=ALG_metadata,
-    publisher_wallet=alice_wallet,
-    services=[ALG_access_service],
-    data_token_address=ALG_datatoken.address)
+    metadata = ALG_metadata,
+    publisher_wallet = alice_wallet,
+    services = [ALG_access_service],
+    data_token_address = ALG_datatoken.address)
 
     trusted_algorithms.add_publisher_trusted_algorithm(DATA_ddo, ALG_ddo.did, config.metadata_cache_uri)
     ocean.assets.update(DATA_ddo, publisher_wallet = alice_wallet)
 
-    bob_wallet = Wallet(
-        ocean.web3,
-        bob_wallet_address,
-        config.block_confirmations,
-        config.transaction_timeout)
-
     DATA_datatoken.transfer(bob_wallet.address, to_wei(5), from_wallet = alice_wallet)
     ALG_datatoken.transfer(bob_wallet.address, to_wei(5), from_wallet = alice_wallet)
 
-#Start Compute Job
-@app.route("/alpha/1.0/computejob/<string:bob_wallet_address>/<string:alice_wallet_address>/<string:data_url>/<string:algo_url>", methods=["GET"], endpoint='compute_job')
-def compute_job(bob_wallet_address, alice_wallet_address, data_url, algo_url):
+    return jsonify(f"DATA_datatoken = '{DATA_datatoken}' ALG_datatoken = '{ALG_datatoken}'")
 
-    alice_wallet = Wallet(ocean.web3, alice_wallet_address, 
-    config.block_confirmations, config.transaction_timeout)
+#Start Compute Job
+@app.route("/alpha/1.0/computejob", methods=["GET"], endpoint='compute_job')
+def compute_job():
 
     DATA_metadata = {
         "main": {
             "type": "dataset",
             "files": [
         {
-            "url": data_url,
+            "url": Dataset_Url,
             "index": 0,
             "contentType": "text/text"
         }
@@ -343,7 +298,7 @@ def compute_job(bob_wallet_address, alice_wallet_address, data_url, algo_url):
     DATA_service_attributes = {
     "main": {
         "name": "DATA_dataAssetAccessServiceAgreement",
-        "creator": alice_wallet_address,
+        "creator": alice_wallet.address,
         "timeout": 3600 * 24,
         "datePublished": "2019-12-28T10:55:11Z",
         "cost": 1.0,}
@@ -353,7 +308,7 @@ def compute_job(bob_wallet_address, alice_wallet_address, data_url, algo_url):
     
     DATA_compute_service = Service(
         service_endpoint=provider_url,
-        service_type=ServiceTypes.CLOUD_COMPUTE,
+        service_type = ServiceTypes.CLOUD_COMPUTE,
         attributes=DATA_service_attributes)
 
     DATA_datatoken = ocean.create_data_token('DATA1', 'DATA1', alice_wallet, blob=ocean.config.metadata_cache_uri)
@@ -383,7 +338,7 @@ def compute_job(bob_wallet_address, alice_wallet_address, data_url, algo_url):
         },
         "files": [
 	  {
-	    "url": algo_url,
+	    "url": Algorithm_Url,
 	    "index": 0,
 	    "contentType": "text/text",
 	  }
@@ -416,12 +371,6 @@ def compute_job(bob_wallet_address, alice_wallet_address, data_url, algo_url):
     trusted_algorithms.add_publisher_trusted_algorithm(DATA_ddo, ALG_ddo.did, config.metadata_cache_uri)
     ocean.assets.update(DATA_ddo, publisher_wallet = alice_wallet)
 
-    bob_wallet = Wallet(
-        ocean.web3,
-        bob_wallet_address,
-        config.block_confirmations,
-        config.transaction_timeout)
-
     DATA_datatoken.transfer(bob_wallet.address, to_wei(5), from_wallet = alice_wallet)
     ALG_datatoken.transfer(bob_wallet.address, to_wei(5), from_wallet = alice_wallet)
 
@@ -432,12 +381,6 @@ def compute_job(bob_wallet_address, alice_wallet_address, data_url, algo_url):
 
     compute_service = DATA_DDO.get_service('compute')
     algo_service = ALG_DDO.get_service('access')
- 
-    bob_wallet = Wallet(
-        ocean.web3,
-        bob_wallet_address,
-        config.block_confirmations,
-        config.transaction_timeout)
 
     dataset_order_requirements = ocean.assets.order(
     DATA_did, bob_wallet.address, service_type = compute_service.type)
@@ -475,7 +418,23 @@ def compute_job(bob_wallet_address, alice_wallet_address, data_url, algo_url):
 
     return jsonify(f"job_id = '{job_id}'")
 
-# Checks to see if the name of the package is the run as the main package.
-#if __name__ == "__main__":
-	# Runs the Flask application only if the publish_dataset.py file is being run.
-	#app.run()
+#Monitor Computing Job
+
+#ocean.compute.status(DATA_did, job_id, bob_wallet)
+
+#result = ocean.compute.result_file(DATA_did, job_id, 0, bob_wallet)
+
+#model = pickle.loads(result) 
+
+#X0_vec = numpy.linspace(-5., 10., 15)
+#X1_vec = numpy.linspace(0., 15., 15)
+#X0, X1 = numpy.meshgrid(X0_vec, X1_vec)
+#b, c, t = 0.12918450914398066, 1.5915494309189535, 0.039788735772973836
+#u = X1 - b*X0**2 + c*X0 - 6
+#r = 10.*(1. - t) * numpy.cos(X0) + 10
+#Z = u**2 + r
+
+#fig, ax = pyplot.subplots(subplot_kw={"projection": "3d"})
+#ax.scatter(X0, X1, model, c="r", label="model")
+#pyplot.title("Data + model")
+#pyplot.show()
