dd3d9f9d6372531b12b20735bf2d681d905c37a7
Datatera <100047030+DatateraTechnology@users.noreply.github.com>
2022-03-27 23:38:06 +0200

    Update and rename __init__.py to app.py

--- HandleApproach/app.py
+++ HandleApproach/app.py
@@ -1,5 +1,4 @@
 import azure.functions as func
-from FlaskApp import app
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the WSGI handler.
